863cd545b3947eb503e6ae5fad085efa9c357339
TrueMerk <merkuteug@gmail.com>
2023-03-12 03:22:21 +0300

    init

---
+++ Assets/Scripts/Gameplay/Entities/Player/WheatPickUper.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace SarrrGames.GoldenRush.Gameplay.Entities.Player
+{
+    public class WheatPickUper : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            var wheat = other.GetComponent<Wheat>();
+        
+            if (wheat != null)
+            {
+                var o = other.gameObject;
+                var localScale = o.transform.localScale;
+                localScale =
+                    new Vector3(localScale.x/2, localScale.y/2, localScale.z/2);
+                o.transform.localScale = localScale;
+                wheat.HasBeenCut();
+            }
+        }
+        
+    }
+}
